acc956585c315e927ce064a00130bfddde347a23
Amir Patel <31699548+Amirpatel89@users.noreply.github.com>
2017-10-26 15:57:22 -0400

    movie posters-express

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config/config');
+var request = require('request');
+
+
+
+
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	request.get(nowPlayingUrl, (error, response, movieData)=>{
+		var parsedData = JSON.parse(movieData);
+		console.log(parsedData);
+		res.render('index', {
+			parsedData: parsedData.results,
+			imageBaseUrl: imageBaseUrl
+		});
+	});
+});
+
+module.exports = router;
+
+
